72ed23d9eb157c0e863199782466a79d5d918047
Peter Goedeke <peterbgoedeke@gmail.com>
2021-05-15 22:48:59 +1200

    feat: added weightings functions

--- library.py
+++ library.py
@@ -12,7 +12,27 @@ class Library:
         self.resources.append(word_counts)
 
     def get_frequencies(self) -> Counter:
-        return reduce(lambda a, c: a.update(c) or a, self.resources)
+        freqs = reduce(lambda a, c: a.update(c) or a, self.resources)
+        # self.combine_cases(freqs)
+        return freqs
 
     def get_ubiquities(self) -> Counter:
-        return reduce(lambda a, c: a.update(c.keys()) or a, [Counter()] + self.resources)
+        ubiqs = reduce(lambda a, c: a.update(c.keys()) or a, [Counter()] + self.resources) # type: Counter
+        # self.combine_cases(ubiqs)
+        return ubiqs
+
+    def get_frequency_weightings(self) -> Dict[str, float]:
+        freqs = self.get_frequencies()
+        return { c[0]: 1 - i / len(freqs) for i, c in enumerate(freqs.most_common())}
+
+    def get_ubiquity_weightings(self) -> Dict[str, float]:
+        ubiqs = self.get_ubiquities()
+        num_buckets = ubiqs.most_common(1)[0][1]
+        buckets = [[] for _ in range(num_buckets)] # type: List[List[str]]
+        for key in ubiqs:
+            val = ubiqs[key]
+            buckets[num_buckets - (val - 1) - 1].append(key)
+
+        return dict(reduce(lambda a, c: a + c,
+            [[(e, 1 - i / len(buckets)) for e in l] for i, l in enumerate(buckets)]
+        ))
