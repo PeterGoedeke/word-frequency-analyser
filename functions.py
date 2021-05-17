7cb36e9808b10d6ef82062abddee048da9894a01
Peter Goedeke <peterbgoedeke@gmail.com>
2021-05-17 19:00:52 +1200

    feat: get_article

--- functions.py
+++ functions.py
@@ -23,3 +23,17 @@ class GermanLanguage:
                     gender = gender_articles[gender_result[0]]
                     genders[word] = gender
         self.dictcc = genders
+
+    def get_article(self, word: str) -> Optional[str]:
+        gd_gender = gd.get_gender(word)
+        if gd_gender:
+            return gender_articles[gd_gender]
+        
+        word = word.lower()
+        if word in self.dictcc:
+            return self.dictcc[word]
+        else:
+            ss_form = word.replace('ß', 'ss')
+            if ss_form in self.dictcc:
+                return self.dictcc[ss_form]
+        return None
